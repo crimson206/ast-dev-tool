c0f93e113f0a12d6cdc88565e19be12a91ee369a
crimson206 <sisung.kim1@gmail.com>
2024-06-27 19:22:16 +0200

    refactored many functions added tests

---
+++ test/example_module.py
@@ -0,0 +1,16 @@
+# test_module.py
+
+
+def test_function():
+    return "This is a test function"
+
+
+TEST_CONSTANT = 42
+
+
+class TestClass:
+    def __init__(self):
+        self.value = "Test value"
+
+    def test_method(self):
+        return self.value
